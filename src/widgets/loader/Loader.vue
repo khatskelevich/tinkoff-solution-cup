4633877bf1062c79cf47d77147fbcc2436b18ab9
Alexey Khatskelevich <hatskelevich.alesha@gmail.com>
2023-04-22 13:33:58 +0300

    feat: создан Loader

---
+++ src/widgets/loader/Loader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="loader-container">
+    <img class="loader" alt="Wallet Loader" src="./system-outline-5-wallet.gif">
+    <h3>Walleter</h3>
+  </div>
+</template>
+
+<style scoped>
+.loader-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.loader {
+  width: 50px;
+  height: 50px;
+}
+</style>
