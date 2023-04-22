c66ac92c50ff809439c2b09a50a2405f9839bf45
Alexey Khatskelevich <hatskelevich.alesha@gmail.com>
2023-04-22 13:01:02 +0300

    chore: поправлена архитектура

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '../widgets/HelloWorld.vue'
+
 </script>
 
 <template>
--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import MyButton from "../shared/MyButton.vue";
 
 defineProps<{ msg: string }>()
 
@@ -16,6 +17,9 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
+
+  <my-button theme="primary">Добавить</my-button>
+  <my-button>Добавить</my-button>
 
   <p>
     Check out
