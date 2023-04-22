71748c48439e13a452c89b596d6c5c4c349f43dd
Alexey Khatskelevich <hatskelevich.alesha@gmail.com>
2023-04-22 13:35:59 +0300

    feat: реализован suspense загрузки

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from '../widgets/HelloWorld.vue'
+import HelloWorld from '../pages/HelloWorld.vue'
+import Loader from "../widgets/loader/Loader.vue";
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <suspense>
+    <HelloWorld/>
+
+    <template #fallback>
+      <loader/>
+    </template>
+  </suspense>
 </template>
 
 <style scoped>
